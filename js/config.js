04c388a9cc7646496b7a32f091e951b33f79f53f
Ashika V <ashikaveeranna22@gmail.com>
2025-11-25 17:57:18 +0530

    Delete js directory

--- js/config.js
+++
@@ -1,4 +0,0 @@
-export const API_KEY = "236ea648";
-export const BASE_URL = "https://api.themoviedb.org/3";
-export const IMG_PATH = "https://image.tmdb.org/t/p/w500";
-export const YT_BASE = "https://www.youtube.com/embed/";
